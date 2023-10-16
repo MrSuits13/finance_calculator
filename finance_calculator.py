7a15d8735418117fa66570b4ff3c9249ef1ef6ba
MrSuits13 <107210589+MrSuits13@users.noreply.github.com>
2023-10-16 07:19:04 +0200

    Completed

--- finance_calculator.py
+++ finance_calculator.py
@@ -21,13 +21,28 @@ if calculator_type.lower() == "investment":
     money_invested = float(input("Enter the amount of money that you are depositing?: "))
     interest_rate = float(input("Enter the interest rate?: "))
     number_of_years = int(input("Enter the number of years you plan on investing the money?: "))
-    interest = input("Choose the type of interest simple/compound?: ")
-    if interest.lower() == "simple":
+    interest_type = input("Choose the type of interest simple/compound?: ")
+    # creating the if statement for simple and compound
+    if interest_type.lower() == "simple":
         money_receive = money_invested * (1 + (interest_rate/100) * number_of_years)
         print(f"The amount you will receive after {number_of_years} years is R {money_receive} ")
-    elif interest.lower() == "compound":
-        money_receive = money_invested * (math.pow((1 + interest_rate),number_of_years))
-        print(f"The amount you will receive after {number_of_years} years is R {money_receive} ")
+    elif interest_type.lower() == "compound":
+        money_receive = money_invested * (math.pow(1 + (interest_rate / 100), number_of_years))
+    #rounding to two decimals
+        money_receive_rounded = round(money_receive, 2)
+        print(f"The amount you will receive after {number_of_years} years is R {money_receive_rounded} ")
     else:
-        print("That is not a valid interest option!")
+        print("Invalid Option!")
+# lets do the bond repayment calculator 
+# lets start by creating the variables and calculating the answer using the formula 
+elif calculator_type.lower() == "bond":
+    present_value = int(input("Enter the present value of the house?: "))
+    interest_rate = float(input("Enter the interest rate?: "))
+    monthly_interest_rate = ( interest_rate / 100 ) / 12
+    number_of_months = int(input("Enter the number of months you plan to repay the bond?: "))
+    repayment = (monthly_interest_rate * present_value)/(1 - (1 + monthly_interest_rate)**(-number_of_months))
+# lets round the answer 2 decimal places 
+    rounded_repayment = round(repayment, 2)
+# printing the montly repayment amount
+    print(f"You will have a monthly repayment of R{rounded_repayment} per month")
 
